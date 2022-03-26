452e5616cb605d1bd0f5e90ac246beb45ba35b9d
iimKoala <90398208+iimKoala@users.noreply.github.com>
2022-03-26 13:56:39 -0400

    Created Platform.h

---
+++ LE4_HayeAlikhani/GAME1017_EmptyEngine/PlatformPlayer.cpp
@@ -0,0 +1,60 @@
+#include "PlatformPlayer.h"
+
+PlatformPlayer::PlatformPlayer(SDL_Rect s, SDL_FRect d)
+{
+}
+
+void PlatformPlayer::Update()
+{
+}
+
+void PlatformPlayer::Render()
+{
+}
+
+void PlatformPlayer::Stop()
+{
+}
+
+void PlatformPlayer::StopX()
+{
+}
+
+void PlatformPlayer::StopY()
+{
+}
+
+void PlatformPlayer::SetAccelX(double a)
+{
+}
+
+void PlatformPlayer::SetAccelY(double a)
+{
+}
+
+void PlatformPlayer::SetX(float x)
+{
+}
+
+void PlatformPlayer::SetY(float y)
+{
+}
+
+void PlatformPlayer::SetGrounded(bool g)
+{
+}
+
+bool PlatformPlayer::IsGrounded()
+{
+    return false;
+}
+
+double PlatformPlayer::GetVelX()
+{
+    return 0.0;
+}
+
+double PlatformPlayer::GetVelY()
+{
+    return 0.0;
+}
